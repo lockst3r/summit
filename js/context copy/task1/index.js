af0abf8e42c85a4f172c9ab4e7724319830bd4c3
lockst3r <ushakovqq@gmail.com>
2020-12-28 13:11:26 +0200

    add

--- js/context copy/task1/index.js
+++ js/context copy/task1/index.js
@@ -1,5 +1,6 @@
 "use strick";
 // option 1
+/*
 const items = document.querySelectorAll('.list-item');
 items.forEach(item=> {
 item.addEventListener('click', handleClick);
@@ -9,14 +10,30 @@ item.addEventListener('click', handleClick);
 function handleClick (){
 console.log('clicked');
 }
-
+*/
 //option 2
 const item =  document.querySelector('.list');
 item.addEventListener('click', handleClick2);
 
 
 function handleClick2 (event){
-console.log(event);
-  console.log('list clicked');
+if(event.target.className !== 'list-item'){
+return;
+};
+  console.log(event.target.textContent);
 }
 
+//WEB FLOW !!!!!
+// 1.get data from server ===> [{}, {}, {}];
+// 2.render()!!
+// 3.handle event
+// 4.
+
+
+// algo
+//1. find task
+//2. update find fill
+//3. render
+function handleClick3(){
+
+}
