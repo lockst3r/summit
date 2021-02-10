db9b52fb064bfc368ff3da9922425bec13bbc7ef
lockst3r <ushakovqq@gmail.com>
2021-02-10 14:10:28 +0200

    add

--- routing/src/User.jsx
+++ routing/src/User.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const User = () => {
+const User = ({ match }) => {
+  const [userAvatar, setUserAvatar] = useState(null);
+  const [userName, setUserName] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
+
+  const getUserData = userId => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(resp => resp.json())
+      .then(userData => {
+        const { avatar_url, name, location } = userData;
+        setUserAvatar(avatar_url);
+        setUserName(name);
+        setUserLocation(location);
+      });
+  };
+
+  useEffect(() => getUserData(match.params.userId), []);
+
+  if (!userAvatar || !userName || !userLocation) {
+    return null;
+  }
   return (
     <div className="user">
-      <img
-        alt="User Avatar"
-        src="https://avatars1.githubusercontent.com/u/9919?v=4"
-        className="user__avatar"
-      />
+      <img alt="User Avatar" src={userAvatar} className="user__avatar" />
       <div className="user__info">
-        <span className="user__name">GitHub</span>
-        <span className="user__location">San Francisco,CA</span>
+        <span className="user__name">{userName}</span>
+        <span className="user__location">{userLocation}</span>
       </div>
     </div>
   );
