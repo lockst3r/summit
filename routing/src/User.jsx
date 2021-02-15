15327b960a11d2b6275fa833a50c3146305cc841
lockst3r <ushakovqq@gmail.com>
2021-02-15 12:56:41 +0200

    aadd

--- routing/src/User.jsx
+++ routing/src/User.jsx
@@ -1,32 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const User = ({ match }) => {
-  const [userAvatar, setUserAvatar] = useState(null);
-  const [userName, setUserName] = useState(null);
-  const [userLocation, setUserLocation] = useState(null);
+const User = props => {
+  const [userInfo, setUserInfo] = useState({
+    avatar: null,
+    name: null,
+    location: null,
+  });
 
-  const getUserData = userId => {
+  const { userId } = useParams();
+
+  useEffect(() => {
     fetch(`https://api.github.com/users/${userId}`)
       .then(resp => resp.json())
       .then(userData => {
         const { avatar_url, name, location } = userData;
-        setUserAvatar(avatar_url);
-        setUserName(name);
-        setUserLocation(location);
+        setUserInfo({
+          avatar: avatar_url,
+          name: name,
+          location: location,
+        });
       });
-  };
+  }, [userId]);
 
-  useEffect(() => getUserData(match.params.userId), []);
+  // callback
+  // input: -
+  // output: undef, func
+  const { avatar, name, location } = userInfo;
 
-  if (!userAvatar || !userName || !userLocation) {
+  if (!avatar || !name || !location) {
     return null;
   }
   return (
     <div className="user">
-      <img alt="User Avatar" src={userAvatar} className="user__avatar" />
+      <img alt="User Avatar" src={avatar} className="user__avatar" />
       <div className="user__info">
-        <span className="user__name">{userName}</span>
-        <span className="user__location">{userLocation}</span>
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
       </div>
     </div>
   );
